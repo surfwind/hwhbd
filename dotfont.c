37128e60ef1083c0c1820ae8cb335662a31dcd62
surfwind <ben60841@hotmail.com>
2020-08-05 21:00:21 +0800

    re format

--- dotfont.c
+++ dotfont.c
@@ -1,65 +1,68 @@
 #include <stdio.h>
-void main()
+#include <stdlib.h>
+
+int main(void)
 {
-    FILE *inf,*inf1,*rd, *out;
+    int i, j, k;
+    int idx = 0;
     unsigned char a[72];
-    unsigned char;
-    unsigned b;
-    int i,j,k,l=0;
+    unsigned char b;
     unsigned char c[513];
-    /* set et3 font path here */
-    /* if you use dos, change '/' to '\\' also */
-    inf=fopen("C:/Users/Ben/Desktop/spcfont.15","rb");
-    inf1=fopen("C:/Users/Ben/Desktop/stdfont.15f","rb");
-    out=fopen("out.txt","w");
-    if(inf==NULL)
-        exit(0);
-    printf("請輸入要轉換的文字:");
-    scanf("%s",c);
-    while(c[l]!='\0')
-    {
-        b=c[l]*256+c[l+1];
-        if (c[l+1]>0xa0)
-            b-=34;
-        if(b>=0xa440)
-        {
-            b-=(c[l]-0xa4)*99;
-            fseek(inf1,(long)(b-0xa440)*30,SEEK_SET);
-            rd=inf1;
+    FILE *fpr = NULL;
+    FILE *fpr2 = NULL;
+    FILE *fpw = NULL;
+    FILE *fp;
+    
+    fpr = fopen("spcfont.15", "rb");
+    if (!fpr)
+        exit(1);
+    fpr2 = fopen("stdfont.15f", "rb");
+    if (!fpr2)
+        exit(1);
+    fpw = fopen("out.txt", "w");
+    if (!fpw)
+        exit(1);
+    
+    printf("input:");
+    scanf("%s", c);
+    while (c[idx] != '\0') {
+        b = (c[idx] << 8) + c[idx + 1];
+        if (c[idx + 1] > 0xa0)
+            b -= 34;
+        if (b >= 0xa440) {
+            b -= (c[idx] - 0xa4) * 99;
+            fseek(fpr2, (long)(b - 0xa440) * 30, SEEK_SET);
+            fp = fpr2;
+        } else {
+            b -= (c[idx] - 0xa1) * 99;
+            fseek(fpr, (long)(b - 0xa140) * 30, SEEK_SET);
+            fp = fpr;
         }
-        else
-        {
-            b-=(c[l]-0xa1)*99;
-            fseek(inf,(long)(b-0xa140)*30,SEEK_SET);
-            rd=inf;
-        }
-        for(i=0;i<30;i++)
-            fscanf(rd,"%c",&a[i]);
-        printf("\n%c%c\n15",c[l],c[l+1]);
-        for(i=0;i<15;i++)
-        {
+        for (i = 0; i < 30; i++)
+            fscanf(fp, "%c", &a[i]);
+        printf("\n%c%c\n15", c[idx], c[idx + 1]);
+        for (i = 0; i < 15; i++) {
             printf("\n");
-            fprintf(out,"\n");
-            for(j=0;j<2;j++)
-                for(k=0;k<8;k++)
-                {
-                    if(a[i*2+j] > 127)
-                    {
+            fprintf(fpw, "\n");
+            for (j = 0; j < 2; j++) {
+                for (k = 0; k < 8; k++) {
+                    if (a[i * 2 + j] > 127) {
                         printf("1");
-                        fprintf(out, "1");
-                    }
-                    else
-                    {
+                        fprintf(fpw, "1");
+                    } else {
                         printf("0");
-                        fprintf(out, "0");
+                        fprintf(fpw, "0");
                     }
-                    a[i*2+j]<<=1;
-                };
-        };
-        l+=2;
-    };
+                    a[i * 2 + j] <<= 1;
+                }
+            }
+        }
+        idx += 2;
+    }
     printf("\n");
-    fclose(inf);
-    fclose(inf1);
-    fclose(out);
+    
+    fclose(fpr);
+    fclose(fpr2);
+    fclose(fpw);
+    return 0;
 }
